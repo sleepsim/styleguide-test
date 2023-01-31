9ef6715091e339f5a3c1b3c9e18561043bcbec02
sleepsim <91400544+sleepsim@users.noreply.github.com>
2023-01-31 00:39:19 -0800

    a bit of js and flexbnox

--- P1-Style_Guide_Template/js/scripts.js
+++ P1-Style_Guide_Template/js/scripts.js
@@ -1,2 +1,22 @@
-var navigation1 = document.getElementById("nav1");
+// var navigation1 = document.getElementById.bind("nav1");
 
+var navigation1 = function(id){
+    return document.getElementById("nav1");
+}
+
+function tester(){
+    document.getElementById("nav1").style.backgroundColor = "black";
+    // console.log("sdfsdfds");
+}
+
+
+
+
+/* Javascript sources
+
+
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_style_backgroundcolor
+
+
+
+*/
